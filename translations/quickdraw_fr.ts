cc3d5875003dc8d7397e583863454f7f8214bcaf
Sébastien Maes <nautilussebastien@gmail.com>
2018-12-28 23:07:32 +0100

    [FIX] breakline issue in french translation

--- translations/quickdraw_fr.ts
+++ translations/quickdraw_fr.ts
@@ -11,7 +11,7 @@
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="139"/>
-        <location filename="../src/mainwindow.cpp" line="199"/>
+        <location filename="../src/mainwindow.cpp" line="198"/>
         <source>Warning</source>
         <translation>Attention</translation>
     </message>
@@ -19,190 +19,190 @@
         <location filename="../src/mainwindow.cpp" line="140"/>
         <source>The image has been modified.
 Do you want to save your changes ?</source>
-        <translation>L&apos;image a été modifiée.\nVoulez-vous sauvegarder vos changements ?</translation>
+        <translation>L&apos;image a été modifiée.&lt;br/&gt;Voulez-vous sauvegarder vos changements ?</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="186"/>
+        <location filename="../src/mainwindow.cpp" line="185"/>
         <source>/untitled.</source>
         <translatorcomment>Nom par défaut pour sauvegarder une nouvelle image</translatorcomment>
         <translation>/sans titre.</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="187"/>
-        <location filename="../src/mainwindow.cpp" line="204"/>
+        <location filename="../src/mainwindow.cpp" line="186"/>
+        <location filename="../src/mainwindow.cpp" line="203"/>
         <source>Save As</source>
         <translation>Enregistrer sous</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="188"/>
-        <location filename="../src/mainwindow.cpp" line="205"/>
+        <location filename="../src/mainwindow.cpp" line="187"/>
+        <location filename="../src/mainwindow.cpp" line="204"/>
         <source>%1 Files (*.%2);; All Files(*)</source>
         <translation>Fichiers %1 (*.%2);; Tous les fichiers(*)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="199"/>
+        <location filename="../src/mainwindow.cpp" line="198"/>
         <source>The file format has been &lt;b&gt;modified&lt;/b&gt;.
  Your image was previously saved in the </source>
-        <translation>Le format de l&apos;image a été &lt;b&gt;modifié&lt;/b&gt;\nVotre image était précédemment sauvegardée dans le format </translation>
+        <translation>Le format de l&apos;image a été &lt;b&gt;modifié&lt;/b&gt;.&lt;br/&gt;Votre image était précédemment sauvegardée dans le format </translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="200"/>
+        <location filename="../src/mainwindow.cpp" line="199"/>
         <source> format.
  A new file will be created.</source>
-        <translation>.\n Un nouveau fichier sera créé.</translation>
+        <translation>.&lt;br/&gt; Un nouveau fichier sera créé.</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="227"/>
+        <location filename="../src/mainwindow.cpp" line="226"/>
         <source>Open File</source>
         <translation>Ouvrir un fichier</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="249"/>
+        <location filename="../src/mainwindow.cpp" line="248"/>
         <source>Format selector</source>
         <translation>Sélecteur de format</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="284"/>
+        <location filename="../src/mainwindow.cpp" line="283"/>
         <source>Pen Width</source>
         <translation>Largeur du stylo</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="285"/>
+        <location filename="../src/mainwindow.cpp" line="284"/>
         <source>Select pen width :</source>
         <translation>Sélectionner la largeur du stylo :</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="296"/>
+        <location filename="../src/mainwindow.cpp" line="295"/>
         <source>About </source>
         <translation>À propos de </translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="297"/>
+        <location filename="../src/mainwindow.cpp" line="296"/>
         <source>&lt;p&gt;&lt;b&gt;Quickdraw&lt;/b&gt; is a simple painting program made by &lt;b&gt;Sébastien Maes&lt;/b&gt;&lt;/p&gt;</source>
         <translation>&lt;p&gt;&lt;b&gt;QuickDraw&lt;/b&gt; est une application simple de dessin créée par &lt;b&gt;Sébastien Maes&lt;/b&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="356"/>
+        <location filename="../src/mainwindow.cpp" line="355"/>
         <source>New image</source>
         <translation>Nouvelle image</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="361"/>
+        <location filename="../src/mainwindow.cpp" line="360"/>
         <source>&amp;Open...</source>
         <translation>&amp;Ouvrir...</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="366"/>
+        <location filename="../src/mainwindow.cpp" line="365"/>
         <source>Close image</source>
         <translation>Fermer l&apos;image</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="369"/>
+        <location filename="../src/mainwindow.cpp" line="368"/>
         <source>Save</source>
         <translation>Enregistrer</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="377"/>
+        <location filename="../src/mainwindow.cpp" line="376"/>
         <source>%1...</source>
         <translation>%1...</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="386"/>
+        <location filename="../src/mainwindow.cpp" line="385"/>
         <source>&amp;Pen Color...</source>
         <translation>Couleur du &amp;stylo...</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="389"/>
+        <location filename="../src/mainwindow.cpp" line="388"/>
         <source>Pen &amp;Width...</source>
         <translation>&amp;Largeur du stylo...</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="392"/>
+        <location filename="../src/mainwindow.cpp" line="391"/>
         <source>Clear Screen</source>
         <translation>Tout effacer</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="393"/>
+        <location filename="../src/mainwindow.cpp" line="392"/>
         <source>Ctrl+L</source>
         <translation>Ctrl+L</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="397"/>
+        <location filename="../src/mainwindow.cpp" line="396"/>
         <source>Print</source>
         <translation>Imprimer</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="402"/>
+        <location filename="../src/mainwindow.cpp" line="401"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="406"/>
+        <location filename="../src/mainwindow.cpp" line="405"/>
         <source>About</source>
         <translation>À propos</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="410"/>
+        <location filename="../src/mainwindow.cpp" line="409"/>
         <source>About Qt</source>
         <translation>À propos de Qt</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="417"/>
+        <location filename="../src/mainwindow.cpp" line="416"/>
         <source>Select Mode</source>
         <translation>Mode Sélection</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="419"/>
+        <location filename="../src/mainwindow.cpp" line="418"/>
         <source>Ctrl+W</source>
         <translation>Ctrl+W</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="423"/>
+        <location filename="../src/mainwindow.cpp" line="422"/>
         <source>Draw Mode</source>
         <translation>Mode Dessin</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="426"/>
+        <location filename="../src/mainwindow.cpp" line="425"/>
         <source>Ctrl+D</source>
         <translation>Ctrl+D</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="430"/>
+        <location filename="../src/mainwindow.cpp" line="429"/>
         <source>Fill Mode</source>
         <translation>Mode Remplissage</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="432"/>
+        <location filename="../src/mainwindow.cpp" line="431"/>
         <source>Ctrl+F</source>
         <translation>Ctrl+F</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="436"/>
+        <location filename="../src/mainwindow.cpp" line="435"/>
         <source>Erase Mode</source>
         <translation>Mode Effacement</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="438"/>
+        <location filename="../src/mainwindow.cpp" line="437"/>
         <source>Ctrl+E</source>
         <translation>Ctrl+E</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="451"/>
+        <location filename="../src/mainwindow.cpp" line="450"/>
         <source>&amp;Save As</source>
         <translation>&amp;Enregistrer sous</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="455"/>
+        <location filename="../src/mainwindow.cpp" line="454"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="463"/>
+        <location filename="../src/mainwindow.cpp" line="462"/>
         <source>&amp;Options</source>
         <translation>&amp;Options</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="469"/>
+        <location filename="../src/mainwindow.cpp" line="468"/>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
@@ -215,44 +215,44 @@ Do you want to save your changes ?</source>
         <translation>Nouvelle image</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="23"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="27"/>
         <source>Pixel Size</source>
         <translation>Taille en pixels</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="38"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="42"/>
         <source>Width</source>
         <translation>Largeur</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="61"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="65"/>
         <source>Height</source>
         <translation>Hauteur</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="84"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="88"/>
         <source>Maintain aspect ratio</source>
         <translation>Conserver les proportions</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="94"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="98"/>
         <source>Settings</source>
         <translation>Paramètres</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="100"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="104"/>
         <source>Background</source>
         <translation>Fond</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="107"/>
         <location filename="../ui/forms/newimagedialog.ui" line="111"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="115"/>
         <location filename="../src/newimagedialog.cpp" line="88"/>
         <source>White</source>
         <translation>Blanc</translation>
     </message>
     <message>
-        <location filename="../ui/forms/newimagedialog.ui" line="116"/>
+        <location filename="../ui/forms/newimagedialog.ui" line="120"/>
         <source>Transparent</source>
         <translation>Transparent</translation>
     </message>
